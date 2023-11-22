c063ff13f36fb8a83d9466106f61ccf92b490231
IsaacXB <isachatr@outlook.com>
2023-11-21 21:59:00 -0600

    New REST API Service. GetUsers() method added.

---
+++ REST/REST.API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using REST.API.Models;
+
+namespace REST.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<User>> GetUsers()
+        {
+            return Ok(new List<User> { 
+                new User { 
+                    Id = 1, 
+                    Name = "Test",
+                    Email = "test@gmail.com",
+                    EmailConfirmed = "No",
+                    Password = "abc123$."
+                    
+                } 
+            });
+        }
+    }
+}
