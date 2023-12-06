5499968848cc694d800cdcab532f28880275176d
IsaacXB <isachatr@outlook.com>
2023-12-05 19:28:23 -0600

    Class 4, Part 1. API 2 and DB Context projects added.

---
+++ REST/REST.DBContext/Context/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using REST.Database.Models;
+
+namespace REST.Database.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Usuarios { get; set; }
+    }
+}
---
+++ REST/REST.DBContext/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace REST.Database.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string EmailConfirmed { get; set; } = string.Empty;
+    }
+}
